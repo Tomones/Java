62334d283de7c254d85eafc60a76286a3c8c76a8
Tomones <670978375@qq.com>
2017-09-27 17:53:06 +0800

    ThreadLocal

---
+++ src/main/java/com/lyss/java/concurrent/threadlocal/ConnThreadLocal.java
@@ -0,0 +1,38 @@
+package com.lyss.java.concurrent.threadlocal;
+/**
+ * ThreadLocal 只是针对在同一个线程中来说的
+ */
+public class ConnThreadLocal {
+
+	public static ThreadLocal<String> tLocal = new ThreadLocal<>();
+	
+	public void setTlocal(String value){
+		tLocal.set(value);
+	}
+	public void getLocal(){
+		System.out.println(Thread.currentThread().getName()+":"+this.tLocal.get());
+	}
+	public static void main(String[] args) {
+		final ConnThreadLocal local = new ConnThreadLocal();
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				local.setTlocal("郭涛");
+				local.getLocal();
+			}
+		},"t1").start();
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+//				try {
+//					Thread.sleep(1000);
+//				} catch (InterruptedException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+				local.getLocal();
+			}
+		},"t2").start();
+	}
+}
